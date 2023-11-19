184d8bf5601f91631c4dc71247a91ea703dd7946
Christophe GUIBOURD DE LUZINAIS <39296266+iChris4@users.noreply.github.com>
2023-11-19 19:21:11 +0100

    Add files via upload

--- play.py
+++ play.py
@@ -4,45 +4,42 @@ from utils import resize_image, XboxController, Screenshot
 from termcolor import cprint
 import mss
 import cv2
-import pyvjoy
+import vgamepad as vg
 import time
-from matplotlib import pyplot as plt
 from datetime import datetime
 from PIL import Image
+
+import pygetwindow as gw
+import pyautogui
 
 from train import create_model
 import numpy as np
 
 # Play
 class Actor(object):
-
     def __init__(self):
         # Load in model from train.py and load in the trained weights
         self.model = create_model(keep_prob=1) # no dropout
         self.model.load_weights('model_weights.h5')
 
-        # Init contoller for manual override
+        # Init controller for manual override
         self.real_controller = XboxController()
 
     def get_action(self, obs):
-
         ### determine manual override
         manual_override = self.real_controller.LeftBumper == 1
 
         if not manual_override:
             ## Look
             vec = resize_image(obs)
-            vec = np.expand_dims(vec, axis=0) # expand dimensions for predict, it wants (1,66,200,3) not (66, 200, 3)
+            vec = np.expand_dims(vec, axis=0) # expand dimensions for predict
             ## Think
             joystick = self.model.predict(vec, batch_size=1)[0]
-
         else:
             joystick = self.real_controller.read()
-            joystick[1] *= -1 # flip y (this is in the config when it runs normally)
-
+            joystick[1] *= -1 # flip y
 
         ## Act
-
         ### calibration
         output = [
             float(joystick[0]),
@@ -61,50 +58,69 @@ class Actor(object):
         return output
 
 def get_screenshot():
-        # Get raw pixels from the screen
-        sct = mss.mss()
-        sct_img = sct.grab({"top": Screenshot.OFFSET_Y,
-                            "left": Screenshot.OFFSET_X,
-                            "width": Screenshot.SRC_W,
-                            "height": Screenshot.SRC_H})
+    # Find the window by its name
+    window = gw.getWindowsWithTitle('Chiaki | Stream')[0]  # Adjust the title if necessary
 
-        # Create the Image
-        img = np.array(sct_img)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        return img
+    if window is not None:
+        # Get the window's position and size
+        x, y, width, height = window.left, window.top, window.width, window.height
 
-def play(action, vjoy, old_time):
-    x_value = int(16384 * action[0] + 16384)
-    y_value = int(16384 * action[1] + 16384)
-    vjoy.data.wAxisX = int(hex(x_value), 16)
-    vjoy.data.wAxisY = int(hex(y_value), 16)
+        # Adjust for window borders if necessary
+        border_thickness = 8  # This is a typical value, but it might vary
+        title_bar_height = 30  # This also varies
+
+        with mss.mss() as sct:
+            # Define the region to capture
+            monitor = {"top": y + title_bar_height, "left": x + border_thickness, 
+                       "width": width - 2 * border_thickness, 
+                       "height": height - title_bar_height - border_thickness}
+
+            # Capture the image
+            sct_img = sct.grab(monitor)
+
+            # Convert to an array
+            img = np.array(sct_img)
+            img = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)  # Adjust color format if necessary
+
+            # Display the image
+            #cv2.imshow('Screenshot', img)
+            #cv2.waitKey(1)  # Waits for a key press for 1 millisecond
+
+            return img
+    else:
+        print("Window not found")
+        return None
+
+
+def play(action, gamepad, old_time):
+    x_value = int(32767 * action[0])
+    y_value = int(32767 * action[1])
+
+    # Updating the gamepad values using the correct vgamepad methods
+    gamepad.left_joystick_float(x_value_float=x_value/32767.0, y_value_float=0)
 
     now = int(round(time.time() * 1000))
-    if now - old_time > 200 :
-        #vjoy.data.lButtons = 1 - vjoy.data.lButtons
-        vjoy.data.lButtons = 1
-        vjoy.update()
+    if now - old_time > 200:
+     # gamepad.press_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
+        gamepad.right_trigger_float(value_float=1.0)
+        gamepad.update()
+#        time.sleep(0.1)
+#        gamepad.release_button(button=vg.XUSB_BUTTON.XUSB_GAMEPAD_A)
         return now
-    vjoy.update()
+    gamepad.update()
     return old_time
 
 if __name__ == '__main__':
-
     actor = Actor()
     old_time = int(round(time.time() * 1000))
-    vjoy = pyvjoy.VJoyDevice(1)
-    vjoy.data.wAxisX = 0x4000
-    vjoy.data.wAxisY= 0x4000
-    vjoy.data.lButtons = 0
-    vjoy.update()
-    print('actor ready!')
+    gamepad = vg.VX360Gamepad()
 
+    print('actor ready!')
     print('beginning loop')
 
     while True:
         screenshot = get_screenshot()
         action = actor.get_action(screenshot)
-        old_time = play(action, vjoy, old_time)
+        old_time = play(action, gamepad, old_time)
 
     input('press <ENTER> to quit')
-
